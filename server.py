8c6113072b6ea55576011669321eda44c64e9c4c
Rick Spencer <richard.linger.spencer.3@gmail.com>
2017-10-01 15:35:43 -0400

    calling setup

--- server.py
+++ server.py
@@ -60,6 +60,7 @@ class S(BaseHTTPRequestHandler):
 
 
 def run(server_class=HTTPServer, handler_class=S, port=80):
+    self._setup_GPIO()
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
     print 'Starting httpd...'
