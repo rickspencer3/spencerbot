9ea407c2ed5e4960d32b2b6c06bc49096cd93f06
Rick Spencer <richard.linger.spencer.3@gmail.com>
2018-04-02 14:01:21 -0400

    try to get server fixed up

--- server.py
+++ server.py
@@ -67,7 +67,7 @@ class S(BaseHTTPRequestHandler):
         GPIO.output(wheels["back"]["left"]["reverse"], 0)
 
     
-    def _set_headers(self):
+    def _set_headers(self, type):
         self.send_response(200)
         if type == "htlm":
             self.send_header('Content-type', 'text/html')
@@ -90,7 +90,7 @@ class S(BaseHTTPRequestHandler):
         
     def do_POST(self):
         self._set_headers()
-        data = self.get_post_data()
+        data = self.get_post_data(
         if data["dir"][0] == "F":
             self.forward()
         elif data["dir"][0] == "S":
